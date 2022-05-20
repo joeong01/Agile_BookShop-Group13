f8c5c29c032609973a2f11c28f62e76b0b09751b
Joe Ong <66934953+joeong01@users.noreply.github.com>
2022-05-20 08:32:21 +0800

    Main Files

---
+++ storage/framework/views/56bed5a18c3e847eeed282c189d37706a5a85654.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <style>
+        body {background-color: rgb(95, 220, 254)}
+    </style>
+    <title>Book Booking System</title>
+
+</head>
+<body>
+    <?php echo e(View::make('userHeader')); ?>
+
+    
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <?php echo e(View::make('footer')); ?>
+
+</body>
+</html><?php /**PATH D:\WebDesign\htdocs\BookBookingSystem\resources\views/userHomepage.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/59ba1dcdff122d14cde304502f0e94f399a7bc51.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Book Bokking System-Admin</title>
+
+
+</head>
+<body style="background-color: gainsboro;">
+    <?php echo e(View::make('adminHeader')); ?>
+
+    
+
+    <?php echo e(View::make('footer')); ?>
+
+</body>
+</html><?php /**PATH D:\WebDesign\htdocs\BookBookingSystem\resources\views/adminHomepage.blade.php ENDPATH**/ ?>

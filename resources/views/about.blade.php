48a0869185e077ace68d58d96df3aa24d564a340
terenceteoh23 <tteoh23@yahoo.com>
2022-05-20 10:27:42 +0800

    updated about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -63,23 +63,23 @@
         <h3>Want to know more? Here are some FAQs.</h3>
         <button type="button" class="collapsible">Open Collapsible</button>
         <div class="content">
-            <p>Lorem ipsum...</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat neque quis diam pulvinar maximus. Nulla feugiat placerat eros, id varius velit mollis eu. Mauris id posuere nisi. Sed nec eleifend ligula, at fermentum orci. Vivamus malesuada, nibh id imperdiet auctor, lectus arcu condimentum libero, in venenatis nisi dui eget ex. Phasellus arcu mi, varius sit amet faucibus eleifend, ultrices in lacus. Mauris id viverra massa, vitae finibus lectus.</p>
         </div>
         <button type="button" class="collapsible">Open Collapsible</button>
         <div class="content">
-            <p>Lorem ipsum...</p>
+            <p>Aenean bibendum scelerisque leo vel aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et consectetur nisl. Donec tempor lectus lacus, vel mollis velit efficitur et. Vestibulum ullamcorper risus vel bibendum sodales. Vestibulum ac dignissim turpis. Nullam vulputate, mi eget imperdiet viverra, mauris risus dictum ante, vel laoreet lacus urna vel ante. Fusce justo libero, euismod a efficitur varius, tempus euismod nulla. Nullam egestas egestas neque, vel consequat ligula. Nam quis lectus nec augue imperdiet tristique. Mauris auctor pretium libero eget placerat. Vivamus a enim augue. Duis et pharetra ante. Etiam vulputate, tortor ut varius commodo, metus nunc suscipit sem, sagittis rutrum turpis diam vel odio. Maecenas vel urna semper, consectetur turpis vel, malesuada urna.</p>
         </div>
         <button type="button" class="collapsible">Open Collapsible</button>
         <div class="content">
-            <p>Lorem ipsum...</p>
+            <p>Maecenas rhoncus odio non velit efficitur cursus. Nulla tristique arcu et diam mollis, ac pulvinar augue tempor. Phasellus quis metus nec nunc condimentum viverra id ut tellus. Mauris sagittis faucibus posuere. Curabitur ultricies leo quis nunc varius dapibus. Vivamus pulvinar placerat cursus. Phasellus eu congue lorem. Sed pretium eget eros et ornare. Sed mauris velit, vestibulum ac nibh et, vehicula luctus purus. Proin maximus faucibus turpis ut molestie.</p>
         </div>
         <button type="button" class="collapsible">Open Collapsible</button>
         <div class="content">
-            <p>Lorem ipsum...</p>
+            <p>Ut magna felis, tincidunt non augue nec, mollis ultrices tellus. Fusce turpis nunc, feugiat vitae lacus et, aliquet sollicitudin risus. Proin luctus nunc orci, at ultrices lectus rhoncus et. Praesent accumsan mauris aliquam eros dictum, vel auctor dui volutpat. Nam elementum in risus quis ullamcorper. Aenean sodales est est, ac elementum diam aliquam quis. Nullam sodales, augue in faucibus pulvinar, ex dui rutrum est, quis vehicula sem mi id felis. Sed laoreet augue eu nisi mollis facilisis. Praesent eget risus nec dui congue facilisis. Aliquam a orci sodales, sagittis quam eu, sodales libero. Integer mattis hendrerit nibh, quis maximus lectus aliquam sed. Pellentesque vel malesuada sem.</p>
         </div>
         <button type="button" class="collapsible">Open Collapsible</button>
         <div class="content">
-            <p>Lorem ipsum...</p>
+            <p>Maecenas non libero nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean faucibus erat eu lectus consequat malesuada. Curabitur aliquam nisl id nunc tempor tristique. Etiam ut arcu consequat, sodales ante a, suscipit orci. Pellentesque et dui nisi. Nam vestibulum ligula placerat metus viverra venenatis faucibus ac ex. Cras eget enim in diam consequat posuere. Nullam convallis commodo mi eu vestibulum. Mauris sagittis facilisis nisi. Cras ultrices ex at neque mattis vehicula. Curabitur sit amet ligula dolor. Donec eleifend sapien sed dolor blandit consectetur. Nam suscipit non ipsum in feugiat.</p>
         </div>
     </div>
     

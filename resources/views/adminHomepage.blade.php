099a7fda9b599902e694fdcc7d4f80a1c37909f5
Joe Ong <66934953+joeong01@users.noreply.github.com>
2022-05-20 20:05:37 +0800

    main file

--- resources/views/adminHomepage.blade.php
+++ resources/views/adminHomepage.blade.php
@@ -4,14 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Book Bokking System-Admin</title>
+    <title>Book Booking System-Admin</title>
+    <style>
+        .home{
+            background-color: grey;
+            margin: 1%;
+            padding: 1%;
+            padding-left: 2%;
+            padding-right: 2%;
+        
+        }
 
+        #page-container{
+            position: relative;
+            min-height: 100vh;
+        }
+
+    </style>
 
 </head>
-<body style="background-color: gainsboro;">
+<body style="background-color: rgb(173, 173, 173);">
     {{ View::make('adminHeader') }}
-    
-
+    <div class="home">
+        <h1>Welcome it's time for work!!!!!</h1>
+        <h2>Will update later, still elearning about the Database</h2>
+    </div>
     {{ View::make('footer') }}
 </body>
 </html>

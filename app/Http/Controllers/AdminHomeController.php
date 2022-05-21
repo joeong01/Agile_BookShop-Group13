bf7d1bc223d48d8eaac9185b531fedfec7eeabc7
terenceteoh23 <tteoh23@yahoo.com>
2022-05-22 00:47:43 +0800

    changes to fix conflict

--- app/Http/Controllers/AdminHomeController.php
+++ app/Http/Controllers/AdminHomeController.php
@@ -5,7 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class AdminHomeController extends Controller
 {
-    
+    public function __invoke()
+    {
+        $stock = DB:: select('select b.ISBN_13, b.bookName, s.stockLevel
+                                from stock s ,book b
+                                where b.ISBN_13 = s.ISBN_13');
+        return view('adminHomepage', compact("stock"));
+    }
 }
--- app/Http/Controllers/UserHomeController.php
+++ app/Http/Controllers/UserHomeController.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class UserHomeController extends Controller
 {
-    
+    public function __invoke()
+    {
+        $stock = DB:: select('select * from book');
+        return view('userHomepage', compact("stock"));
+    }
 }

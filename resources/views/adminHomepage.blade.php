672c020e92dde71d3c6aaf7c15d845e27194de4a
terenceteoh23 <tteoh23@yahoo.com>
2022-05-22 00:41:54 +0800

    changes to fix conflict

--- resources/views/adminHomepage.blade.php
+++ resources/views/adminHomepage.blade.php
@@ -10,7 +10,25 @@
 </head>
 <body style="background-color: gainsboro;">
     {{ View::make('adminHeader') }}
-    
+    <div class="home">
+        <div class="Display">
+            <table>                
+                <tr>
+                    <th colspan="3" style="color: rgb(0, 0, 0); font-size: 30px;text-align:center" >Stock Level</th>
+                </tr>
+                <tr>
+                    <th>IBSN_13</th>
+                    <th>Book Title</th>
+                    <th>Stock Level</th>
+            @foreach ($stock as $row)
+                <tr>
+                    <td>{{ $row->ISBN_13 }}</td>
+                    <td>{{ $row->bookName }}</td>
+                    <td style="text-align: center">{{ $row->stockLevel }}</td>
+            @endforeach
+            </table>
+        </div>
+    </div>
 
     {{ View::make('footer') }}
 </body>
--- resources/views/userHomepage.blade.php
+++ resources/views/userHomepage.blade.php
@@ -10,6 +10,52 @@
 <body style="background-color: gainsboro;">
     {{ View::make('userHeader') }}
 
+    <div class="home">
+        <div class="Display">
+            <table>
+                <tr>
+                    <th colspan="4" style="color: rgb(0, 0, 0); font-size: 30px;text-align:center" >New Release</th>
+                </tr>
+                <tr>
+                    <td rowspan="3" ><img src="{{ url('/Picture/Dark Psycho.jpg') }}" /></td>
+                    <th style="width: 530px;">DARK PSYCHOLOGY: Dark Hypnosis Technique To Manipulation Human Psychology, Deception, Subliminal Persuasion And Mind Control</th>
+                    <td rowspan="3" ><img src="{{ url('/Picture/Screenshot 2022-05-20 103621.png') }}"/></td>
+                    <th>THE ART OF A LAWYER - CROSS EXAMINATION | ADVOCACY | COURTMANSHIP</th>
+                </tr>
+                <tr>
+                    <th>By :Ryan Watson<br><br></th>
+                    <th>By :Chief Justice M. Monir , Dr. B. Malik</th>
+                </tr>
+                <tr>
+                    <th>Price : 60.00</th>
+                    <th>Price : 250.00</th>
+                </tr>   
+            </table>
+        </div>
+        <br>
+        <div class="Display">
+            <table >
+                <tr>
+                    <th colspan="4" style="color: rgb(0, 0, 0); font-size: 30px;text-align:center">Top Sale</th>
+                </tr>
+                <tr>
+                    <td rowspan="3"><img src="{{ url('/Picture/Screenshot 2022-05-20 104415.png') }}" /></td>
+                    <th>MODERN ADVOCACY - MORE PERSPECTIVES FROM<br> SINGAPORE</th>
+                    <td rowspan="3"><img src="{{ url('/Picture/Screenshot 2022-05-20 104600.png') }}" /></td>
+                    <th>FUNDAMENTALS OF TRIAL TECHNIQUE 4TH AUSTRALIAN ED</th>
+                </tr>
+                <tr>
+                    <th>By :Elearnor Wong, Lok Vi Ming,<br> The Honourable Justice Vinodh Coomaraswamy</th>
+                    <th>By : Thomas A Mauet, Les A. McCrimmon</th>
+                </tr>
+                <tr>
+                    <th>Price : 310.00</th>
+                    <th>Price : 350.00</th>
+                </tr>   
+            </table>
+        </div>
+    </div>
+    
     {{ View::make('footer') }}
 </body>
 </html>

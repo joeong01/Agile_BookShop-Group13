bc30cea976a53e11d04f2cdf845f263c875eeac4
P18010231 <p18010231@student.newinti.edu.my>
2022-05-30 14:29:28 +0800

    Minor Changes

--- resources/views/shoppingCartItems.blade.php
+++ resources/views/shoppingCartItems.blade.php
@@ -125,7 +125,7 @@
         </table>
         <div class="subtotal">
             <span class="text">Subtotal</span>
-            <span class="price">&dollar;<?=$subtotal?></span>
+            <!--<span class="price">&dollar;<?=$subtotal?></span>-->
         </div>
         <div class="buttons">
             <input type="submit" value="Update" name="update">

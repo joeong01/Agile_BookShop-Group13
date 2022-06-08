38b4e0679335f9a67c656a5d1203ff17e200e294
terenceteoh23 <tteoh23@yahoo.com>
2022-06-08 19:49:17 +0800

    test unit added

---
+++ tests/Unit/DatabaseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertDatabaseHas('book',['ISBN_13'=>'1945673581649']);
+    }
+}
